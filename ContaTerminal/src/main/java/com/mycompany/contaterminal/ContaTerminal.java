58664a372332b076f450abf1d352cac2e91dcb44
emmil <emmil@By_Emmy>
2024-09-20 23:19:14 -0300

    Funcionando, mas os acentos dão erro

--- ContaTerminal/src/main/java/com/mycompany/contaterminal/ContaTerminal.java
+++ ContaTerminal/src/main/java/com/mycompany/contaterminal/ContaTerminal.java
@@ -2,9 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.terminaljava;
+package com.mycompany.contaterminal;
 
-import java.util.Locale;
 import java.util.Scanner;
 /**
  *
@@ -14,23 +13,21 @@ public class ContaTerminal {
 
     public static void main(String[] args) {
         
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        Scanner scanner = new Scanner(System.in);
         
         System.out.println("Digite seu nome completo: ");
         String nome = scanner.nextLine();
         
-        System.out.println("Digite o número da sua conta: ");
-        int numero = scanner.nextInt();
+        System.out.println("\nDigite o número da sua conta: ");
+        int numero = Integer.parseInt(scanner.nextLine());
         
-        System.out.println("Digite sua agência: ");
-        String agencia = scanner.next();
+        System.out.println("\nDigite sua agência: ");
+        String agencia = scanner.nextLine();
         
-        System.out.println("Digite seu saldo: ");
-        double saldo = scanner.nextDouble();
-        //TODO: conhecer e importar a classe scanner
-        //Exibir as mensagens para o nosso usuário
-        //Obter pela Scanner os valores digitados no terminal
-        //Exibir a mensagem conta criada
+        System.out.println("\nDigite seu saldo: ");
+        double saldo = Double.parseDouble(scanner.nextLine());
+        
+        System.out.println("\nOlá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", a conta é " + numero + " e seu saldo de R$" + saldo + " já esta disponível para saque.");
         
     }
 }
